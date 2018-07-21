bed61a014f3c20303d3a30ea65deae6c22bbab8b
cackharot <cackharot@gmail.com>
2018-07-21 18:07:33 +0530

    update login screen ui

--- src/modules/loginScreen.js
+++ src/modules/loginScreen.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Platform, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import {
     ImageBackground,
     RefreshControl,
@@ -10,6 +12,7 @@ import { connect } from 'react-redux';
 import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 import { Container, Text, Button, Right, Left, Body, Content, Icon } from 'native-base';
 import { showMainApp } from '../appNav';
+import { httpClient } from '../actions/httpClient';
 
 class LoginScreen extends Component {
     constructor(props) {
@@ -19,34 +22,7 @@ class LoginScreen extends Component {
         };
     }
 
-    _signOut = async () => {
-        console.log('Sigining out user');
-        try {
-            await GoogleSignin.revokeAccess();
-            await GoogleSignin.signOut();
-            this.setState({ user: null });
-        } catch (error) {
-            this.setState({
-                error,
-            });
-        }
-    };
-
-    getCurrentUser = async () => {
-        try {
-            const user = await GoogleSignin.currentUserAsync();
-            if (user && user.name) {
-                console.log(`Got signed in user ${user.name}`);
-                showMainApp();
-            }
-            this.setState({ user });
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
     componentDidMount = async () => {
-        await this.getCurrentUser();
     }
 
     _signIn = async () => {
@@ -55,13 +31,18 @@ class LoginScreen extends Component {
             this.setState({ user });
             if (user && user.name) {
                 console.log(`Got signed in user ${user.name}`);
+                storage.save({key: 'loginState', data: user});
+                storage.save({key: 'idToken', data: user.idToken});
+                httpClient.defaults.headers.common['Authorization'] = `Bearer ${user.idToken}`;
                 showMainApp();
             }
         } catch (error) {
             if (error.code === 'CANCELED') {
+                this.setState({error: 'Please sigin with a Google Account'});
                 // user cancelled the login flow
             } else {
                 // some other error happened
+                this.setState({error: 'Please sigin with a Google Account'});
             }
         }
     };
@@ -71,28 +52,80 @@ class LoginScreen extends Component {
         return (
             <Container>
                 <Content contentContainerStyle={{ flexBasis: '100%' }}>
-                    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-                        {user && user.name && (
-                            <View>
-                                <Text>{user.name}</Text>
-                                <Button onPressOut={this._signOut}>
-                                    <Text>SignOut</Text>
-                                </Button>
-                            </View>
-                        )}
-                        {!user &&
-                            <GoogleSigninButton
-                                style={{ width: 312, height: 48 }}
-                                size={GoogleSigninButton.Size.Icon}
-                                color={GoogleSigninButton.Color.Light}
-                                onPress={this._signIn} />
-                        }
+                <ImageBackground source={require('../images/login_bg.jpg')} style={styles.imageBackdrop} >
+                    <LinearGradient colors={['rgba(80,80,135, 0.9)', 'rgba(80,80,130, 0.8)', 'rgba(80,80,140, 0.7)']} style={styles.linearGradient} />
+                    <View style={styles.loginContainer}>
+                    <Icon name="pluse" ios="ios-pulse" style={styles.logo} />
+                    <Text style={styles.title}>Foodbeazt</Text>
+                    <Text style={styles.desc}>Store administrator App</Text>
+                <Button style={styles.loginBtn} onPressOut={this._signIn}>
+                <Icon name="logo-googleplus" style={styles.gplus}/>
+                <Text style={styles.btnText}>Sign In with Google</Text>
+                </Button>
                     </View>
+                </ImageBackground>
                 </Content>
             </Container>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    loginContainer: {
+        flex: 1,
+        // backgroundColor: '#4F4D8D',
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageBackdrop: {
+        width: null,
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    linearGradient: {
+        top: 0,
+        left: 0,
+        right: 0,
+        height: '100%',
+        position: 'absolute'
+    },
+    logo: {
+        fontSize: 80,
+        // color: 'transparent',
+        color: 'white',
+        fontWeight: '600'
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: 'white',
+        marginTop: 10,
+        marginBottom: 5
+    },
+    desc: {
+        fontSize: 30,
+        fontWeight: '200',
+        color: 'white',
+        marginBottom: 20
+    },
+    loginBtn: {
+        width: 250,
+        backgroundColor: 'white',
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 15
+    },
+    btnText: {
+        color: '#4F4D8D'
+    },
+    gplus: {
+        color: '#4F4D8D'
+    }
+});
 
 function mapStateToProps(state, ownProps) {
     return {};
